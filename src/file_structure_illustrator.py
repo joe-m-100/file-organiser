bc92cc316f83390272cc0c5cf2339cd5bbfb33e3
Joe Meredith <joe.meredith100@gmail.com>
2026-08-29 13:45:18 +0100

    File Structure Illustrator refactor + bug fixes + test cases

--- src/file_structure_illustrator.py
+++ src/file_structure_illustrator.py
@@ -12,26 +12,25 @@ class FileStructureIllustrator:
         self.__structure = structure
 
     def generate_illustration(self):
-        self.__illustration = self.illustrate_directory(self.__structure, 0)
+        self.__illustration = self.illustrate_directory(self.__structure, [])
 
-    def illustrate_directory(self, directory: dict[str, list[Path|str|dict]], depth: int) -> list[str]:
+    def illustrate_directory(self, directory: dict[str, list[Path|str|dict]], is_last_child_stack: list[bool]) -> list[str]:
         output = []
 
         # Loop through each directory
         for directory_name, directory_contents in directory.items():
 
             # Format and add the directory name to output
-            formatted_directory_name = f'{directory_name.capitalize()}/'
-            self.format_illustration_line(item_name=formatted_directory_name, depth=depth, last_item=False)
+            capitalized_directory_name = f'{directory_name.capitalize()}/'
+            formatted_directory_name = self.format_illustration_line(item_name=capitalized_directory_name, is_last_child_stack=is_last_child_stack)
 
             output.append(formatted_directory_name)
 
             # Loop through directory contents, looking for files & subdirectories
             for item in directory_contents:
                 if type(item) == dict:
-                    nested_dir = self.illustrate_directory(item, depth + 1)
-
-                    nested_dir[0] = self.format_illustration_line(item_name=nested_dir[0], depth=(depth + 1), last_item=(len(nested_dir) == 1))
+                    last = item == directory_contents[-1]
+                    nested_dir = self.illustrate_directory(item, [*is_last_child_stack, last])
 
                     output.extend(nested_dir)
                     continue
@@ -43,28 +42,32 @@ class FileStructureIllustrator:
                     item_name = item
 
                 last = item == directory_contents[-1]
-                formatted_line = self.format_illustration_line(item_name, depth + 1, last_item=last)
+                formatted_line = self.format_illustration_line(item_name, [*is_last_child_stack, last])
 
                 output.append(formatted_line)
 
         return output
 
     @staticmethod
-    def format_illustration_line(item_name: str, depth: int, last_item: bool) -> str:
-        if depth < 1:
+    def format_illustration_line(item_name: str, is_last_child_stack: list[bool]) -> str:
+        if not is_last_child_stack:
             return item_name
 
-        if last_item:
+        if is_last_child_stack[-1]:
             item_name = f'└── {item_name}'
         else:
             item_name = f'├── {item_name}'
 
-        if depth > 1:
-            spaces = (depth - 1) * 3 * ' '
-            item_name = f'│{spaces}{item_name}'
+        prefix = ''
+        for i in range(len(is_last_child_stack) - 1):
+            if is_last_child_stack[i]:
+                prefix += '    '
+            else:
+                prefix += '│   '
+            
 
-        return item_name
+        return f'{prefix}{item_name}'
 
 
-    def display(self):
+    def display(self) -> None:
         print(self.__illustration)
--- tests/test_file_structure_illustrator.py
+++ tests/test_file_structure_illustrator.py
@@ -2,18 +2,92 @@ import pytest
 from src.file_structure_illustrator import FileStructureIllustrator
 
 @pytest.mark.parametrize('input, expected', [
+    ([], 'example.txt'),
+    ([False], '├── example.txt'),
+    ([True], '└── example.txt'),
+    ([False, True], '│   └── example.txt'),
+    ([True, False], '    ├── example.txt'),
+    ([False, True, False, False], '│       │   ├── example.txt'),
+    ([True, False, True, True], '    │       └── example.txt'),
+])
+def test_format_illustration_line(input, expected):
+    # Arrange
+    illustrator = FileStructureIllustrator({})
+
+    # Act
+    actual = illustrator.format_illustration_line('example.txt', input)
+
+    # Assert
+    assert actual == expected, f"Illustration strings do not match."
+
+@pytest.mark.parametrize('input, expected', [
     ({}, []),
     (
         {'documents': ['file1.txt', 'file2.txt']},
-        ['Documents/', '├── file1.txt', '└── file2.txt']
+        [
+            'Documents/',
+            '├── file1.txt',
+            '└── file2.txt'
+        ]
     ),
     (
         {'documents': ['file1.txt', 'file2.txt'], 'images': ['image1.png']},
-        ['Documents/', '├── file1.txt', '└── file2.txt', 'Images/', '└── image1.png']
+        [   
+            'Documents/',
+            '├── file1.txt',
+            '└── file2.txt',
+            'Images/',
+            '└── image1.png'
+        ]
     ),
     (
         {'documents': [{'images': ['image1.png']}, 'file1.txt', 'file2.txt']},
-        ['Documents/', '├── Images/', '│   └── image1.png', '├── file1.txt', '└── file2.txt']
+        [
+            'Documents/',
+            '├── Images/',
+            '│   └── image1.png',
+            '├── file1.txt',
+            '└── file2.txt'
+        ]
+    ),
+    (
+        {'documents': ['file1.txt', 'file2.txt', {'images': ['image1.png']}]},
+        [
+            'Documents/',
+            '├── file1.txt',
+            '├── file2.txt',
+            '└── Images/',
+            '    └── image1.png',
+        ]
+    ),
+    (
+        {'documents': [{'images': ['image1.png']}, 'file1.txt', 'file2.txt', {'music': [{'archived': ['song1.mp3']}, 'song2.mp3']}, 'file3.txt']},
+        [
+            'Documents/', 
+            '├── Images/',
+            '│   └── image1.png',
+            '├── file1.txt',
+            '├── file2.txt',
+            '├── Music/',
+            '│   ├── Archived/',
+            '│   │   └── song1.mp3',
+            '│   └── song2.mp3', 
+            '└── file3.txt',
+        ]
+    ),
+    (
+        {'documents': [{'images': ['image1.png']}, 'file1.txt', 'file2.txt', {'music': [{'archived': ['song1.mp3']}]}, 'file3.txt']},
+        [
+            'Documents/', # []
+            '├── Images/', # False
+            '│   └── image1.png', # False True
+            '├── file1.txt', # False
+            '├── file2.txt', # False
+            '├── Music/', # False
+            '│   └── Archived/', # False True
+            '│       └── song1.mp3', # False True True
+            '└── file3.txt', # True
+        ]
     ),
 ])
 def test_file_structure_illustration(input, expected):
